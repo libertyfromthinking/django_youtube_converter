b87636e4480ba3307fdcf2a527a12f1f30dacc48
libertyfromthinking <libertyfromthinking@gmail.com>
2019-10-10 16:35:05 +0900

    modify wsgi.py

--- my_youtube/wsgi.py
+++ my_youtube/wsgi.py
@@ -15,4 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_youtube.settings')
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
